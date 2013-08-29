39ed5a16eaeecb065d949e2593d9b1d01c5b2306
Ilias Bilionis <ebilionis@gmail.com>
2013-08-29 14:11:40 +0300

    Added orthpol to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 
 
-from distutils.core import setup
+from numpy.distutils.core import setup
+from numpy.distutils.core import Extension
+import os
+import glob
 
 
 setup(name='Bayesian Exploration Statistical Toolbox',
       author='Ilias Bilionis',
       version='0.0',
+      ext_modules=[Extension('best.core.orthpol',
+                             glob.glob(os.path.join('src', 'orthpol',
+                                                    '*.f')))],
       packages=['best', 'best.core', 'best.misc', 'best.domain',
                 'best.maps', 'best.linalg', 'best.random', 'best.rvm',
                 'best.gp', 'best.design', 'best.uq', 'best.inverse'])
