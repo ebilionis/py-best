7f18a5c27730a3ee0ff379119137dd88e0a5ec39
Ilias Bilionis <ebilionis@gmail.com>
2013-09-02 16:24:07 +0300

    Fixed lob.f.

--- src/orthpol/dlob.f
+++ src/orthpol/dlob.f
@@ -1,17 +1,28 @@
 c
 c
 
-      subroutine dlob(n,dalpha,dbeta,dleft,dright,dzero,dweigh,
+      subroutine dlob(n,alpha,beta,aleft,right,zero,weight,
      *ierr,de,da,db)
 c
 c This is a double-precision version of the routine  lob.
 c
-      double precision dleft,dright,depsma,dp0l,dp0r,dp1l,dp1r,dpm1l,
-     *dpm1r,ddet,dalpha(*),dbeta(*),dzero(*),dweigh(*),de(*),da(*),
+      double precision aleft,right,depsma,dp0l,dp0r,dp1l,dp1r,dpm1l,
+     *dpm1r,ddet,alpha(*),beta(*),zero(*),weight(*),de(*),da(*),
      *db(*),d1mach
 c
-c The arrays  dalpha,dbeta,dzero,dweigh,de,da,db  are assumed to have
+c The arrays  alpha,beta,zero,weight,de,da,db  are assumed to have
 c dimension  n+2.
+cf2py integer intent(hide),depend(alpha) :: n=len(alpha)
+cf2py real intent(in) :: alpha
+cf2py real intent(in),depend(n),dimension(n) :: beta
+cf2py real intent(in) :: aleft
+cf2py real intent(in) :: right
+cf2py real intent(out),depend(n),dimension(n+2) :: zero
+cf2py real intent(out),depend(n),dimension(n+2) :: weight
+cf2py real intent(hide),depend(n),dimension(n+2) :: de
+cf2py real intent(hide),depend(n),dimension(n+2) :: da
+cf2py real intent(hide),depend(n),dimension(n+2) :: db
+cf2py integer intent(out) :: ierr
 c
       depsma=d1mach(3)
 c
@@ -20,8 +31,8 @@ c
       np1=n+1
       np2=n+2
       do 10 k=1,np2
-        da(k)=dalpha(k)
-        db(k)=dbeta(k)
+        da(k)=alpha(k)
+        db(k)=beta(k)
    10 continue
       dp0l=0.d0
       dp0r=0.d0
@@ -32,13 +43,13 @@ c
         dp0l=dp1l
         dpm1r=dp0r
         dp0r=dp1r
-        dp1l=(dleft-da(k))*dp0l-db(k)*dpm1l
-        dp1r=(dright-da(k))*dp0r-db(k)*dpm1r
+        dp1l=(aleft-da(k))*dp0l-db(k)*dpm1l
+        dp1r=(right-da(k))*dp0r-db(k)*dpm1r
    20 continue
       ddet=dp1l*dp0r-dp1r*dp0l
-      da(np2)=(dleft*dp1l*dp0r-dright*dp1r*dp0l)/ddet
-      db(np2)=(dright-dleft)*dp1l*dp1r/ddet
-      call dgauss(np2,da,db,depsma,dzero,dweigh,ierr,de)
+      da(np2)=(aleft*dp1l*dp0r-right*dp1r*dp0l)/ddet
+      db(np2)=(right-aleft)*dp1l*dp1r/ddet
+      call dgauss(np2,da,db,depsma,zero,weight,ierr,de)
       return
       end
 
--- src/orthpol/slob.f
+++ src/orthpol/slob.f
@@ -1,13 +1,13 @@
 c
 c
-      subroutine lob(n,alpha,beta,aleft,right,zero,weight,ierr,e,a,b)
+      subroutine slob(n,alpha,beta,aleft,right,zero,weight,ierr,e,a,b)
 c
-c Given  n  and a measure  dlambda, this routine generates the 
+c Given  n  and a measure  dlambda, this routine generates the
 c (n+2)-point Gauss-Lobatto quadrature formula
-c 
+c
 c   integral over supp(dlambda) of f(x)dlambda(x)
 c
-c      = w(0)f(x(0)) + sum from k=1 to k=n of w(k)f(x(k))  
+c      = w(0)f(x(0)) + sum from k=1 to k=n of w(k)f(x(k))
 c
 c              + w(n+1)f(x(n+1)) + R(n;f).
 c
@@ -16,7 +16,7 @@ c =w(k), k=0,1,...,n,n+1. The user has to supply the recursion
 c coefficients  alpha(k), beta(k), k=0,1,...,n,n+1, for the measure
 c dlambda. The nodes and weights are computed in terms of the
 c eigenvalues and first component of the normalized eigenvectors of
-c a slightly modified Jacobi matrix of order  n+2. The routine calls 
+c a slightly modified Jacobi matrix of order  n+2. The routine calls
 c upon the subroutine  gauss  and the function subroutine  r1mach.
 c
 c   Input:  n - -  the number of interior points in the Gauss-Lobatto
@@ -25,12 +25,12 @@ c           alpha,beta - arrays of dimension  n+2  to be supplied with
 c                  the recursion coefficients  alpha(k-1), beta(k-1),
 c                  k=1,2,...,n+2, of the underlying measure; the
 c                  routine does not use  alpha(n+2), beta(n+2)
-c           aleft,right - the prescribed left and right endpoints 
+c           aleft,right - the prescribed left and right endpoints
 c                  x(0)  and  x(n+1)  of the Gauss-Lobatto formula
 c
-c   Output: zero - an array of dimension  n+2  containing the nodes (in 
+c   Output: zero - an array of dimension  n+2  containing the nodes (in
 c                  increasing order)  zero(k)=x(k), k=0,1,...,n,n+1
-c           weight-an array of dimension  n+2  containing the weights 
+c           weight-an array of dimension  n+2  containing the weights
 c                  weight(k)=w(k), k=0,1,...,n,n+1
 c           ierr - an error flag inherited from the routine  gauss
 c
@@ -41,6 +41,17 @@ c
 c The arrays  alpha,beta,zero,weight,e,a,b  are assumed to have
 c dimension  n+2.
 c
+cf2py integer intent(hide),depend(alpha) :: n=len(alpha)
+cf2py real intent(in) :: alpha
+cf2py real intent(in),depend(n),dimension(n) :: beta
+cf2py real intent(in) :: aleft
+cf2py real intent(in) :: right
+cf2py real intent(out),depend(n),dimension(n+2) :: zero
+cf2py real intent(out),depend(n),dimension(n+2) :: weight
+cf2py real intent(hide),depend(n),dimension(n+2) :: e
+cf2py real intent(hide),depend(n),dimension(n+2) :: a
+cf2py real intent(hide),depend(n),dimension(n+2) :: b
+cf2py integer intent(out) :: ierr
       epsma=r1mach(3)
 c
 c epsma is the machine single precision.
@@ -66,7 +77,7 @@ c
       det=p1l*p0r-p1r*p0l
       a(np2)=(aleft*p1l*p0r-right*p1r*p0l)/det
       b(np2)=(right-aleft)*p1l*p1r/det
-      call gauss(np2,a,b,epsma,zero,weight,ierr,e)
+      call sgauss(np2,a,b,epsma,zero,weight,ierr,e)
       return
       end
 
